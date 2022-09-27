d7f014c12e6964bed96bb74d1fbaafd15e2d6344
Кирилл Дегтярев <kirill.degtyar76@gmail.com>
2022-09-27 18:14:58 +0300

    changes

--- src/js/script.js
+++ src/js/script.js
@@ -16,58 +16,104 @@ inputTerm.addEventListener("input", changeRangeTerm);
 rangeTerm.addEventListener("input", changeInputTerm);
 
 function changeInputPrice(e) {
-    const contribution = (+e.target.value * +10) / 100;
-    inputPrice.value = e.target.value;
+    const value = +e.target.value;
+    const contribution = Math.round((value * +10) / 100);
+    inputPrice.value = Math.round(value);
 
-    percentBody.textContent = "10%";
-    rangeContribution.value = 10;
+    if (rangeContribution.value === 0) {
+        percentBody.textContent = "10%";
+        rangeContribution.value = 10;
+    }
     inputContribution.value = contribution;
-    changePaymentResult(e.target.value, contribution, rangeContribution.value, rangeTerm.value);
-    changeResultPrice(contribution, rangeTerm.value, pricePayment.textContent);
+    changePaymentResult(
+        inputPrice.value,
+        inputContribution.value,
+        rangeContribution.value,
+        inputTerm.value
+    );
+    changeResultPrice(inputContribution.value, rangeTerm.value, pricePayment.textContent);
 }
 
 function changeRangePrice(e) {
-    let value = +e.target.value;
-    const contribution = (value * +10) / 100;
+    const value = +e.target.value;
+    const contribution = Math.round((value * +10) / 100);
     rangePrice.value = value;
-    percentBody.textContent = "10%";
-    rangeContribution.value = 10;
+    if (rangeContribution.value === 0) {
+        percentBody.textContent = "10%";
+        rangeContribution.value = 10;
+    }
     inputContribution.value = contribution;
-    changeResultPrice(contribution, rangeTerm.value, pricePayment.textContent);
+    changePaymentResult(
+        inputPrice.value,
+        inputContribution.value,
+        rangeContribution.value,
+        inputTerm.value
+    );
+    changeResultPrice(inputContribution.value, rangeTerm.value, pricePayment.textContent);
 }
 
 function changeInputContribution(e) {
-    const contribution = (+inputPrice.value * +e.target.value) / 100;
+    const contribution = Math.round((+inputPrice.value * +e.target.value) / 100);
     percentBody.textContent = e.target.value + "%";
     inputContribution.value = contribution;
-    changeResultPrice(contribution, rangeTerm.value, pricePayment.textContent);
+
+    changePaymentResult(
+        inputPrice.value,
+        inputContribution.value,
+        rangeContribution.value,
+        inputTerm.value
+    );
+    changeResultPrice(inputContribution.value, rangeTerm.value, pricePayment.textContent);
 }
 
 function changeRangeContribution(e) {
     const percent = Math.round((+e.target.value * 100) / +inputPrice.value);
     rangeContribution.value = percent;
     percentBody.textContent = percent + "%";
-    changeResultPrice(e.target.value, rangeTerm.value, pricePayment.textContent);
+    changePaymentResult(
+        inputPrice.value,
+        inputContribution.value,
+        rangeContribution.value,
+        inputTerm.value
+    );
+    changeResultPrice(inputContribution.value, rangeTerm.value, pricePayment.textContent);
 }
 
 function changeRangeTerm(e) {
     rangeTerm.value = e.target.value;
+    changePaymentResult(
+        inputPrice.value,
+        inputContribution.value,
+        rangeContribution.value,
+        inputTerm.value
+    );
     changeResultPrice(inputContribution.value, rangeTerm.value, pricePayment.textContent);
 }
 
 function changeInputTerm(e) {
     inputTerm.value = e.target.value;
-    changeResultPrice(inputContribution.value, e.target.value, pricePayment.textContent);
+    changePaymentResult(
+        inputPrice.value,
+        inputContribution.value,
+        rangeContribution.value,
+        inputTerm.value
+    );
+    changeResultPrice(inputContribution.value, rangeTerm.value, pricePayment.textContent);
 }
 
 function changeResultPrice(contribution, term, payment) {
     const resultPrice = document.querySelector(".price-sum__result").firstChild;
-    const price = +contribution + +term * +payment;
+    const price = Math.round(+contribution + +term * +payment.replace(/\s/g, ""));
     resultPrice.textContent = price.toLocaleString();
 }
 
 function changePaymentResult(price, contribution, percent, term) {
-    const payment = +price - +contribution * (+percent / (1 + +percent) - +term - 1);
+    const payment = Math.round(
+        (+price - +contribution) *
+            (((+percent / 100) * Math.pow(1 + +percent / 100, +term)) /
+                Math.pow(1 + +percent / 100, +term - 1))
+    );
+    pricePayment.textContent = payment.toLocaleString();
 }
 
 function getTreaty() {
